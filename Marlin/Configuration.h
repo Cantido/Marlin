67bce1af7390114d8a051f1c32f476b7445c434c
Rosa Richter <robert.c.richter@gmail.com>
2018-04-22 09:59:10 -0500

    Tune extruder thermistor PID

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -370,11 +370,11 @@
   //#define  DEFAULT_Ki 2.25
   //#define  DEFAULT_Kd 440
 
-  // ANET A8 Standard Extruder at 210 Degree Celsius and 100% Fan
-  //(measured after M106 S255 with M303 E0 S210 C8)
-  #define  DEFAULT_Kp 21.0
-  #define  DEFAULT_Ki 1.25
-  #define  DEFAULT_Kd 86.0
+  // Rosa's Frankenstein Daughter
+  // tuned at 200 deg C with "M303 E0 S200 C8", no fan
+  #define  DEFAULT_Kp 20.95
+  #define  DEFAULT_Ki 1.33
+  #define  DEFAULT_Kd 82.95
 
 #endif // PIDTEMP
 
