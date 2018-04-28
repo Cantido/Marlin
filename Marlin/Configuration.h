125e04a5191f3079b6dc5c4282b1b4bbbd79d90a
Rosa Richter <robert.c.richter@gmail.com>
2018-04-28 10:42:38 -0500

    Create a splash screen

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -94,7 +94,7 @@
 // boot image unmodified. For an example have a look at the bq Hephestos 2
 // example configuration folder.
 //
-//#define SHOW_CUSTOM_BOOTSCREEN
+#define SHOW_CUSTOM_BOOTSCREEN
 // @section machine
 
 /**
